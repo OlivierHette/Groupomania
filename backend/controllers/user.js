039426a6bbbd7d00d673129c822a408630ebd21a
Olivier Hette <equipeartgaming@gmail.com>
2022-04-26 19:48:24 +0200

    [FIX] Backend controllers User

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,18 +3,33 @@ const jwt = require('jsonwebtoken')
 const User = require('../models').User
 
 exports.signup = (req, res, next) => {
+    let regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+
+    let email       = req.body.email
+    let username    = req.body.username
+    let pass        = req.body.pass
+    let isAdmin     = req.body.isAdmin
+
     console.log('req body before: \n',req.body)
-    bcrypt.hash(req.body.pass, 10)
-        .then(hash => {
-            const user = {
-                email: req.body.email,
-                username: req.body.username,
-                pass: hash,
-                isAdmin: false
-            }
-            User.create(user)
-                .then(() => res.status(201).json({ message: 'Utilisateur créé avec succès !' }))
-                .catch(error => res.status(400).json({ message: 'Impossible de créer cet utilisateur', error }))
-        })
-        .catch(error => res.status(500).json({ error: "Erreur serveur" }))
+
+    if(email == null || username == null || pass == null) {
+        return res.status(401).json({ error: 'Champs vide' })
+    }
+    if(regex.test(pass)){ 
+        bcrypt.hash(pass, 10)
+            .then(hash => {
+                const user = {
+                    email: email,
+                    username: username,
+                    pass: hash,
+                    isAdmin: isAdmin
+                }
+                User.create(user)
+                    .then(() => res.status(201).json({ message: 'Utilisateur créé avec succès !' }))
+                    .catch(error => res.status(400).json({ message: 'Impossible de créer cet utilisateur', error }))
+            })
+            .catch(error => res.status(500).json({ error: "Erreur serveur" }))
+    } else {
+        return res.status(401).json({ error: 'Le mot de passe doit avoir au moins 8 caractères, un nombre, une minuscule, et une majuscule' })
+    }
 }
