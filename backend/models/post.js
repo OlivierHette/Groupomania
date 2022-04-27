fd927abb846f57ecb75ae85ccdaa319f82fb8c05
Olivier Hette <equipeartgaming@gmail.com>
2022-04-27 15:05:56 +0200

    [ADD] Adding post's Model for database

---
+++ backend/models/post.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+
+      models.Post.belongsTo(models.User, {
+        foreignKey: 'idUsers'
+      })
+    }
+  }
+  Post.init({
+    idUsers: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    imageUrl: DataTypes.STRING,
+    likes: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Post',
+  });
+  return Post;
+};
